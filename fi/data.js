fc5aea0983a8b7411e3767d2da8f0a76cb2ff27b
ponaba1804 <jim0916870422@gmail.com>
2024-05-09 11:41:08 +0800

    563563

--- fi/data.js
+++ fi/data.js
@@ -39,7 +39,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
+    "fullscreenButton": true,
     "viewControlButtons": false
   }
 };
